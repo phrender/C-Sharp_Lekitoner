add7157c69b424b14474f19540aa741435c97fb8
Kevin Berglund <kevteq@gmail.com>
2014-11-24 15:45:26 +0100

    SwitchCase uppdaterad

--- Code/Lektion 09 Switch - Case/SwitchCase_uppgift_2/SwitchCase_uppgift_2/Program.cs
+++ Code/Lektion 09 Switch - Case/SwitchCase_uppgift_2/SwitchCase_uppgift_2/Program.cs
@@ -11,12 +11,33 @@ namespace SwitchCase_uppgift_2
         {
             string strInput = string.Empty;
 
+            Console.Write("Enter a data type: ");
+            strInput = Console.ReadLine();
+
             switch (strInput)
             {
+                case "string":
+                    string strStringValue = strInput.ToString();
+                    Console.WriteLine("Our string value contains: {0} and costs {1} bytes of memory", strStringValue, strStringValue.Length * 2);
+                    break;
 
+                case "int":
+                    int iMyInt = strInput.Length;
+                    Console.WriteLine("Our int has the value of: {0} and costs {1} bytes of memory", iMyInt, sizeof(int));
+                    break;
+
+                case "float":
+                    float fMyFloat = strInput.Length;
+                    Console.WriteLine("Our float has the value of: {0} and costs {1} bytes of memory", fMyFloat, sizeof(float));
+                    break;
+
+                case "double":
+                    int dMydouble = strInput.Length;
+                    Console.WriteLine("Our double has the value of: {0} and costs {1} bytes of memory", dMydouble, sizeof(double));
+                    break;
 
                 default:
-                    Console.WriteLine("");
+                    Console.WriteLine("Either incorrect data type or this probram does not support the given data type.");
                     break;
             };
         }
