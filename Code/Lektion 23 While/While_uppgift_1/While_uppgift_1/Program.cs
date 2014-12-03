e20dacc9f93f74fba6cd12e1d62b47564174bb46
Kevin Berglund <kevteq@gmail,com>
2014-12-03 15:41:56 +0100

    Uppdatering: Kod
    Koden för dagens uppgifter, fixade felet i While_uppgift_2, rad 40,
    programmet innehöll en switch-case som saknade 'default'

---
+++ Code/Lektion 23 While/While_uppgift_1/While_uppgift_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace While_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iCounter = 0;
+            string strContinue = string.Empty;
+
+            while (iCounter < 10)
+            {
+                strContinue = Console.ReadLine();
+
+                if (strContinue.Equals(""))
+                {
+                    iCounter++;
+                    Console.WriteLine("iCount = {0}", iCounter);
+                }
+            }
+        }
+    }
+}
---
+++ Code/Lektion 23 While/While_uppgift_2/While_uppgift_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace While_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bContinue = true;
+            string strPassword = string.Empty;
+            int iNumberOfTries = 3;
+            char chInput = '\n';
+
+            while (bContinue)
+            {
+                Console.Write("Enter password: ");
+                strPassword = Console.ReadLine();
+
+                if (strPassword.Equals(""))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome.");
+
+                    Console.WriteLine("a. Something!\nq. Quit");
+                    Console.Write("Choise: ");
+                    chInput = char.Parse(Console.ReadLine());
+
+                    while (bContinue)
+                    {
+                        switch (chInput)
+                        {
+                            case 'a':
+                                Console.WriteLine("Try again!");
+                                break;
+                            case 'q':
+                                bContinue = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Try something else.");
+                                break;
+                        }
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Wrong Password!");
+                    iNumberOfTries--;
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    if (iNumberOfTries == 0)
+                    {
+                        bContinue = false;
+                    }
+                }
+            }
+        }
+    }
+}
